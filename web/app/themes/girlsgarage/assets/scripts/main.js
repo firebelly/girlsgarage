1908d1c033c2c04a9e5831aac5d6febb0a3737e7
Matt Soria <matt.m.soria@gmail.com>
2016-07-14 16:50:16 -0500

    Fixing nav, sticky, mobile, and regular

--- web/app/themes/girlsgarage/assets/scripts/main.js
+++ web/app/themes/girlsgarage/assets/scripts/main.js
@@ -130,6 +130,9 @@ var FBSage = (function($) {
       $siteHeader.removeClass('sub-menu-active');
     });
 
+    // Duplicate the header logo for the nav
+    $('.site-header .brand').clone().prependTo('.site-nav');
+
     $('<button class="menu-toggle"><span class="lines"></span><svg class="icon icon-circle-stroke" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 61.8 62"><style>.circle-stroke{fill:none;}</style><path id="bottom" class="circle-stroke" d="M1 33c1 15.6 14 28 29.9 28 15.9 0 28.9-12.4 29.9-28"/><path id="top" class="circle-stroke" d="M60.8 29c-1-15.6-14-28-29.9-28C15 1 2 13.4 1 29"/></svg></button>')
       .prependTo('.site-header')
       .on('click', function(e) {
