23d7b33dc531d6496953835eeee4ce715084efa3
Matt Soria <matt.m.soria@gmail.com>
2016-08-03 11:37:05 -0500

    Trying out a mobile nav solution for menu items with child pages

--- web/app/themes/girlsgarage/assets/scripts/main.js
+++ web/app/themes/girlsgarage/assets/scripts/main.js
@@ -257,6 +257,21 @@ var FBSage = (function($) {
     // Duplicate the header logo for the nav
     $('.site-header .brand').clone().prependTo('.site-nav');
 
+    // Add dropdown toggle to menu items with child pages
+    $('.site-nav .menu-item-has-children').each(function() {
+      $(this).prepend('<button class="sub-menu-toggle" aria-hidden="true"></button>');
+    });
+    // Toggle the dropdown when tapped
+    $document.on('click', '.site-nav .sub-menu-toggle', function() {
+      var $thisSubMenu = $(this).closest('.menu-item').find('.sub-menu');
+      if ($thisSubMenu.is('.-toggled')) {
+        $thisSubMenu.removeClass('-toggled');
+      } else {
+        $('.site-nav .sub-menu.-toggled').removeClass('-toggled');
+        $thisSubMenu.addClass('-toggled');
+      }
+    });
+
     $('<button class="menu-toggle"><span class="lines"></span><svg class="icon icon-circle-stroke" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 61.8 62"><style>.circle-stroke{fill:none;}</style><path id="bottom" class="circle-stroke" d="M1 33c1 15.6 14 28 29.9 28 15.9 0 28.9-12.4 29.9-28"/><path id="top" class="circle-stroke" d="M60.8 29c-1-15.6-14-28-29.9-28C15 1 2 13.4 1 29"/></svg></button>')
       .prependTo('.site-header')
       .on('click', function(e) {
