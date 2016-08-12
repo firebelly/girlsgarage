5a2947f2986d6ca4e918e01a17718dd8811def5e
Matt Soria <matt.m.soria@gmail.com>
2016-08-12 17:02:13 -0500

    For the moment falling back to just using the vimeo image without treating it until I figure this shiiii out

--- web/app/themes/girlsgarage/lib/media.php
+++ web/app/themes/girlsgarage/lib/media.php
@@ -71,7 +71,8 @@ function get_header_bg($post_or_image, $remote_img=false, $thumb_id='', $color_o
 
       $background_image = $post_or_image;
       $remote_filename = basename($background_image).PHP_EOL;
-      copy($background_image, $base_dir.$remote_filename);
+      $remote_image = file_get_contents($background_image);
+      file_put_contents($base_dir.$remote_filename, $remote_image);
       $background_image = $base_dir.$remote_filename;
     } else {
       $background_image = $post_or_image;
