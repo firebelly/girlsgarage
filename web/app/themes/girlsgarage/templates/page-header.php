225d6e2e3c195f7288d5594fea023b4d8cd36004
Nate Beaty <nate@clixel.com>
2017-06-21 13:37:12 -0500

    fix errors on 404s

--- web/app/themes/girlsgarage/templates/page-header.php
+++ web/app/themes/girlsgarage/templates/page-header.php
@@ -1,18 +1,19 @@
 <?php
   use Roots\Sage\Titles;
+  $header_bg = '';
   if ($wp_query->is_posts_page) {
     $post = get_post(get_option('page_for_posts'));
   }
-  $header_video = get_post_meta($post->ID, '_cmb2_featured_video', true);
-  if (!$header_video) {
-    $header_bg = \Firebelly\Media\get_header_bg($post, false, '', 'color', 'banner_image');
-  } else {
-    $header_bg = '';
+  if (is_object($post)) {
+    $header_video = get_post_meta($post->ID, '_cmb2_featured_video', true);
+    if (!$header_video) {
+      $header_bg = \Firebelly\Media\get_header_bg($post, false, '', 'color', 'banner_image');
+    }
   }
 ?>
 
 <div class="page-header" <?= $header_bg ?>>
-  <?php if ($header_video): ?>
+  <?php if (!empty($header_video)): ?>
   <div class="background-video-wrapper">
     <video class="background-video" playsinline autoplay muted loop poster="">
       <source src="<?= $header_video ?>" type="video/mp4">
