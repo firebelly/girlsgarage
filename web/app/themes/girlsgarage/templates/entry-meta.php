d89e9e0acac45578c3ac85acf60c690bc5b151ca
poopsplat <matt.m.soria@gmail.com>
2020-03-27 10:48:54 -0500

    Removing published date from project posts

--- web/app/themes/girlsgarage/templates/entry-meta.php
+++ web/app/themes/girlsgarage/templates/entry-meta.php
@@ -1,7 +1,10 @@
+<?php if ($post->post_type != 'project'): ?>
 <div class="meta-section">
   <h6>Published</h6>
   <time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
 </div>
+<?php endif ?>
+
 <?php
   $topics = get_the_terms( $post->ID, 'topic');
 ?>
