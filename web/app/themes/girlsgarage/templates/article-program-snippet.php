c250fa07714868593c83084593f9bd8af6aa4d78
poopsplat <matt.m.soria@gmail.com>
2020-03-27 15:50:55 -0500

    Adjusting formatting of date range in upcoming sessions on homepage

--- web/app/themes/girlsgarage/templates/article-program-snippet.php
+++ web/app/themes/girlsgarage/templates/article-program-snippet.php
@@ -6,7 +6,7 @@
 
 <li>
   <article>
-    <p class="post-meta"><?= date('m/d/y', $program_start); ?><?= $program_start != $program_end ? ' <span>-</span> ' . date('m/d/y', $program_end) : '' ?></p>
+    <p class="post-meta"><?= date('m/d/y', $program_start); ?><?= $program_start != $program_end ? ' <span>to</span> ' . date('m/d/y', $program_end) : '' ?></p>
     <div class="post-heading">
       <h5 class="post-category"><?= $program_type[0]->name ?></h5>
       <h3 class="post-title"><a href="<?= get_permalink($program) ?>"><span><?= $program->post_title ?></span></a></h3>
